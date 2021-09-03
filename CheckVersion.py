5269e1b15789d91e31c7533688f52f0f17d6bc08
Rain981012 <siyuan9@ualberta.ca>
2021-09-02 21:57:31 -0600

    modified self-download

--- CheckVersion.py
+++ CheckVersion.py
@@ -1,8 +1,34 @@
 import requests
+import urllib.request
 
-print(requests.__version__)
+url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
 
-r = requests.get('http://google.com/')
+def printVersion():
+	print("current requests lib version is ",requests.__version__)
 
-print(r)
 
+def getHomepage():
+	page = 'http://google.com/'
+	r = requests.get(page)
+	print(r)
+
+def DownloadItSelf():
+	url = 'https://raw.githubusercontent.com/Rain981012/CMPUT404-Lab1/main/CheckVersion.py'
+	r = requests.get(url)
+
+	with open('/Users/wusiyuan/cmput404/CMPUT404-Lab1/Downloadfile.py', 'wb') as f:
+		f.write(r.content)
+
+	newf = open('/Users/wusiyuan/cmput404/CMPUT404-Lab1/Downloadfile.py')
+
+	lines = newf.read()
+
+	print(lines)
+
+def main():
+	DownloadItSelf()
+	printVersion()
+	getHomepage()
+
+if __name__ == '__main__':
+	main()
